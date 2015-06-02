2622d827e1e3b6b6a6bd072711fc6732c89ca8cc
Phil <plantlife26@gmail.com>
2015-06-02 13:50:06 -0400

    v.01

--- src/Player.java
+++ src/Player.java
@@ -5,11 +5,13 @@ private float battingAverage =.5f;
 private String name = "test Player";
  
 private PlayerState atBat;
+private PlayerState running;
 
 public Player(float battingAvg, String n){
 	battingAverage = battingAvg;
 	name = n;
 	atBat = new AtBat(battingAverage);
+	running = new Running();
 }
 
 public void upAtBat(){
@@ -18,6 +20,10 @@ public void upAtBat(){
 
 public int pitch(Ball b){
 	return atBat.action(b);
+}
+
+public void startRunning(){
+	
 }
 
 public String toString(){
---
+++ src/Running.java
@@ -0,0 +1,18 @@
+
+public class Running implements PlayerState {
+
+
+	
+public Running(float rS){
+
+}
+
+public void start(){
+
+}
+
+public int action(Object o){
+	return 0;
+}
+
+}
