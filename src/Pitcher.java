79cbc2a73cfc82fc22ae92e8cd252a54d3fb560d
Phil <plantlife26@gmail.com>
2015-06-18 14:30:37 -0400

    pitcher stuff

--- src/Pitcher.java
+++ src/Pitcher.java
@@ -1,12 +1,87 @@
 
-public class Pitcher extends Player {
+public class Pitcher extends Player{ 	
 
-int pitchingStat = 1;
+private float zonePer;
+private float fb;
+private float sl;
+private float ct;
+private float cb;
+private float ch;
+private float sf;
+private float kn;
 
-	public Pitcher(float battingAvg, String n, int p) {
-		super(battingAvg, n);
-		pitchingStat = p;
+private float fbV;
+private float slV;
+private float ctV;
+private float cbV;
+private float chV;
+private float sfV;
+private float knV;
+
+
+public Pitcher(String[] playerData, String[] pitcherData) {
+	super(playerData);
 		
-	}
+}
+
+public float getZonePer() {
+	return zonePer;
+}
+
+public float getFb() {
+	return fb;
+}
+
+public float getSl() {
+	return sl;
+}
+
+public float getCt() {
+	return ct;
+}
+
+public float getCb() {
+	return cb;
+}
+
+public float getCh() {
+	return ch;
+}
+
+public float getSf() {
+	return sf;
+}
+
+public float getKn() {
+	return kn;
+}
+
+public float getFbV() {
+	return fbV;
+}
+
+public float getSlV() {
+	return slV;
+}
+
+public float getCtV() {
+	return ctV;
+}
+
+public float getCbV() {
+	return cbV;
+}
+
+public float getChV() {
+	return chV;
+}
+
+public float getSfV() {
+	return sfV;
+}
+
+public float getKnV() {
+	return knV;
+}
 
 }
